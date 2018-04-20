6ba184633c813a39ef1d83d1feb3478b4cec0bc0
xschen <xs0040@gmail.com>
2018-04-20 10:25:18 +0800

    working samples

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # cs-ffmpeg-mp3-converter
-Convert other audio file to mp3 using ffmpeg in .NET
+
+Convert audio files of other formats to mp3 using ffmpeg in .NET
+
+# Install 
+
+```bash
+Install-Package FFMPEG-MP3-Converter -Version 1.0.1
+```
+
+
+# Usage
+
+```cs
+string source_filename="sample.au";
+string target_filename="converted.mp3"; 
+List<string> errors = cs_ffmpeg_mp3_converter.FFMpeg.Convert2Mp3(source_filename, target_filename);
+```
+
+
---
+++ cs-ffmpeg-mp3-converter-samples/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cs_ffmpeg_mp3_converter;
+
+namespace cs_ffmpeg_mp3_converter_samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = Path.Combine(IOUtils.AssemblyDirectory, "..", "..", "..", "blues.00000.au");
+
+
+            string mp3file = "converted.mp3";
+            Console.WriteLine("Converting {0}", file);
+            FFMpeg.Convert2Mp3(file, mp3file);
+            if (!File.Exists(mp3file))
+            {
+                Console.WriteLine("Failed to convert to {0}", mp3file);
+            }
+        }
+    }
+}
